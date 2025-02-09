143f1b92e46d087e6b4005e28feb5e0f3e8c3066
vladislavna <vladislavna@inbox.ru>
2025-02-09 23:13:57 +0400

    []

--- hw10-byteCodes/src/main/java/ru/otus/aop/instrumentation/changer/AnyClass.java
+++
@@ -1,7 +0,0 @@
-package ru.otus.aop.instrumentation.changer;
-
-public class AnyClass {
-    public int summator(int x, int y) {
-        return x + y;
-    }
-}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,6 +2,7 @@ rootProject.name = "otusJava"
 include("hw01-gradle")
 include("hw04-generics")
 include("hw06-annotations")
+include("hw10-byteCodes")
 
 pluginManagement {
     val jgitver: String by settings
