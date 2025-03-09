cdf20bdfcb7b99193dcacff098c206ef64c39efa
vladislavna <vladislavna@inbox.ru>
2025-03-09 12:26:21 +0400

    [hw16]

--- hw16-io/homework/src/main/java/ru/otus/dataprocessor/ResourcesFileLoader.java
+++ hw16-io/homework/src/main/java/ru/otus/dataprocessor/ResourcesFileLoader.java
@@ -1,16 +1,29 @@
 package ru.otus.dataprocessor;
 
-import java.util.Collections;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.json.JsonMapper;
+import java.io.IOException;
 import java.util.List;
 import ru.otus.model.Measurement;
 
 public class ResourcesFileLoader implements Loader {
+    private final String fileName;
+    private final ObjectMapper mapper;
 
-    public ResourcesFileLoader(String fileName) {}
+    public ResourcesFileLoader(String fileName) {
+        this.fileName = fileName;
+        this.mapper = JsonMapper.builder().build();
+    }
 
     @Override
     public List<Measurement> load() {
         // читает файл, парсит и возвращает результат
-        return Collections.emptyList();
+        try {
+            return mapper.readValue(
+                    ResourcesFileLoader.class.getClassLoader().getResource(fileName), new TypeReference<>() {});
+        } catch (IOException e) {
+            throw new FileProcessException(e);
+        }
     }
 }
